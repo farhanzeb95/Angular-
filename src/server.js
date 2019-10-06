c2859fa7813cef1f9c8af00ec1015a13fd46cb0c
farhanzeb95 <farhanzeb95@gmail.com>
2019-10-06 18:52:44 +0200

    fixes in the serving file

--- src/server.js
+++ src/server.js
@@ -6,11 +6,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/Frontend'));
+app.use(express.static(__dirname + '/dist/ROSHAAN-CCTV-FRONTEND'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/Frontend/index.html'));
+res.sendFile(path.join(__dirname+'/ROSHAAN-CCTV-FRONTEND/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
